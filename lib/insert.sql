e966fd954026f977bbeeed9359a7fb91e17c72ba
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-27 20:53:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,18 +1,18 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 1);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 1),
+("Harry Potter", 2, 2);
 
-INSERT INTO subgenres (name) VALUES ("Fantasy");
-INSERT INTO subgenres (name) VALUES ("Magic");
+INSERT INTO subgenres (name) VALUES ("Fantasy"),
+("Magic");
 
-INSERT INTO authors (name) VALUES ("J.R.R. Tolkien");
-INSERT INTO authors (name) VALUES ("J.K. Rowling");
+INSERT INTO authors (name) VALUES ("J.R.R. Tolkien"),
+("J.K. Rowling");
 
-INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
-INSERT INTO books (title, year, series_id) VALUES ("The Two Towers", 1954, 1);
-INSERT INTO books (title, year, series_id) VALUES ("The Return of the King", 1954, 1);
-INSERT INTO books (title, year, series_id) VALUES ("The Sorcerer's Stone", 1997, 2);
-INSERT INTO books (title, year, series_id) VALUES ("The Chamber of Secrets", 1998, 2);
-INSERT INTO books (title, year, series_id) VALUES ("The Goblet of Fire", 2000, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1),
+("The Two Towers", 1954, 1),
+("The Return of the King", 1954, 1),
+("The Sorcerer's Stone", 1997, 2),
+("The Chamber of Secrets", 1998, 2),
+("The Goblet of Fire", 2000, 2);
 
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "The Boy Who Lived", "Wizard", 2, 2),
 ("Hagrid", "The Gentle Giant", "Giant", 2, 2),
