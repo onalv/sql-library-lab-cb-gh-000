85b29f26f98eb569cdba68757b28d4b7ddb877d7
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-27 20:51:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,19 +23,19 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Sar
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Lurtz", "An Army Worthy of Mordor", "Uruk-hai", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Shelob", "She likes to feed", "Giant Spider", 1, 1);
 
-INSERT INTO character_books (character_id, book_id) VALUES (1, 4);
-INSERT INTO character_books (character_id, book_id) VALUES (1, 5);
-INSERT INTO character_books (character_id, book_id) VALUES (1, 6);
-INSERT INTO character_books (character_id, book_id) VALUES (2, 4);
-INSERT INTO character_books (character_id, book_id) VALUES (2, 5);
-INSERT INTO character_books (character_id, book_id) VALUES (2, 6);
-INSERT INTO character_books (character_id, book_id) VALUES (3, 4);
-INSERT INTO character_books (character_id, book_id) VALUES (4, 5);
-INSERT INTO character_books (character_id, book_id) VALUES (5, 1);
-INSERT INTO character_books (character_id, book_id) VALUES (5, 2);
-INSERT INTO character_books (character_id, book_id) VALUES (5, 3);
-INSERT INTO character_books (character_id, book_id) VALUES (6, 1);
-INSERT INTO character_books (character_id, book_id) VALUES (6, 2);
-INSERT INTO character_books (character_id, book_id) VALUES (6, 3);
-INSERT INTO character_books (character_id, book_id) VALUES (7, 1);
-INSERT INTO character_books (character_id, book_id) VALUES (8, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 4),
+(1, 5),
+(1, 6),
+(2, 4),
+(2, 5),
+(2, 6),
+(3, 4),
+(4, 5),
+(5, 1),
+(5, 2),
+(5, 3),
+(6, 1),
+(6, 2),
+(6, 3),
+(7, 1),
+(8, 3);
