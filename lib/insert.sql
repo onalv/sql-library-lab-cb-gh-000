e1c24f0c6ee06a821bc3660a0a62477c9275e8df
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-27 20:51:51 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -14,14 +14,14 @@ INSERT INTO books (title, year, series_id) VALUES ("The Sorcerer's Stone", 1997,
 INSERT INTO books (title, year, series_id) VALUES ("The Chamber of Secrets", 1998, 2);
 INSERT INTO books (title, year, series_id) VALUES ("The Goblet of Fire", 2000, 2);
 
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "The Boy Who Lived", "Wizard", 2, 2);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Hagrid", "The Gentle Giant", "Giant", 2, 2);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Fluffy", "A bit of music puts em right to sleep", "Three-Headed Dog", 2, 2);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Gilderoy Lockhart", "Me!", "Wizard", 2, 2);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Frodo Baggins", "The Bravest Hobbit", "Hobbit", 1, 1);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Saruman", "The White Wizard", "Wizard", 1, 1);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Lurtz", "An Army Worthy of Mordor", "Uruk-hai", 1, 1);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Shelob", "She likes to feed", "Giant Spider", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "The Boy Who Lived", "Wizard", 2, 2),
+("Hagrid", "The Gentle Giant", "Giant", 2, 2),
+("Fluffy", "A bit of music puts em right to sleep", "Three-Headed Dog", 2, 2),
+("Gilderoy Lockhart", "Me!", "Wizard", 2, 2),
+("Frodo Baggins", "The Bravest Hobbit", "Hobbit", 1, 1),
+("Saruman", "The White Wizard", "Wizard", 1, 1),
+("Lurtz", "An Army Worthy of Mordor", "Uruk-hai", 1, 1),
+("Shelob", "She likes to feed", "Giant Spider", 1, 1);
 
 INSERT INTO character_books (character_id, book_id) VALUES (1, 4),
 (1, 5),
